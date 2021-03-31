fc3f2c0ec3948db246f447665b8f7ffc59a8f57a
Jake Chung <luonchung@gmail.com>
2021-03-31 16:45:50 -0700

    initial commit for lab 1 with the code copied and pasted from the source code. LED is not connected use the O scope to look at the output signal

---
+++ MicrocontrollerLab/Lab1/BlinkHelloWorld/BlinkHelloWorld/Debug/main.d
@@ -0,0 +1,43 @@
+main.d main.o: .././main.c \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88pa.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88pa.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ MicrocontrollerLab/Lab1/BlinkHelloWorld/BlinkHelloWorld/main.c
@@ -0,0 +1,19 @@
+// Blink Version 2
+#include <avr/io.h>
+#define F_CPU 1000000UL  // 1 MHz  CPU Clock Frequency
+#include <util/delay.h>  // Include the built in Delay Function
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+int main (void)
+{
+	//1 = output, 0 = input
+	DDRC = 0b00100000; // Set Pin 5 as Output
+	while(1)
+	{
+		sbi(PORTC, 5); // Set pin 5 on PORTC
+		_delay_ms(500); // Delay 500 ms
+		cbi(PORTC, 5); // Clear pin 5 on PORTC
+		_delay_ms(500); // Delay 500 ms
+	}
+	return(0);
+}
