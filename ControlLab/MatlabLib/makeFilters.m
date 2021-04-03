c4186238dacf81b8c0cb22e3d50d6271df4fa5e4
Jake Chung <luonchung@gmail.com>
2021-04-03 16:28:05 -0700

    method to create a 2nd order RC filter

--- ControlLab/MatlabLib/makeFilters.m
+++ ControlLab/MatlabLib/makeFilters.m
@@ -11,5 +11,9 @@ classdef makeFilters
             %function to calculate R for RC filter
             R = 1/(2*pi*fb*C);
         end
+        function R = make2ndRC(fb, C)
+            %function to calculate R for a second order RC filter
+            R = 0.3742/(2*pi*fb*C);
+        end
     end
 end
