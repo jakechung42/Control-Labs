4d55242b1a27374289929f70ac4204805ca186e4
Jake Chung <luonchung@gmail.com>
2021-04-04 14:29:38 -0700

    modify and build to blink PD6

--- MicrocontrollerLab/Lab1/Blink2Program/Blink2Program/main.c
+++ MicrocontrollerLab/Lab1/Blink2Program/Blink2Program/main.c
@@ -2,17 +2,17 @@
 #include <avr/io.h>
 #define F_CPU 1000000UL  // 1 MHz  CPU Clock Frequency
 #include <util/delay.h>  // Include the built in Delay Function
-#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
-#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask)) //set bit in I/O register
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask)) //clear bit in I/O register
 int main (void)
 {
 	//1 = output, 0 = input
-	DDRD = 0b00000000; // Set Pin 5 as Output
+	DDRD = 0b01000000; // Set Pin 5 as Output
 	while(1)
 	{
-		sbi(PORTC, 6); // Set pin 5 on PORTC
+		sbi(PORTD, 6); // Set pin 6 on PORTD
 		_delay_ms(500); // Delay 500 ms
-		cbi(PORTC, 6); // Clear pin 5 on PORTC
+		cbi(PORTD, 6); // Clear pin 6 on PORTD
 		_delay_ms(500); // Delay 500 ms
 	}
 	return(0);
