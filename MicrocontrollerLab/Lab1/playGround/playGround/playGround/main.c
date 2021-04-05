b46e60f1c26c7d4c60d258aa1b99efbf885f0003
Jake Chung <luonchung@gmail.com>
2021-04-04 21:53:08 -0700

    microcontroller playground project

---
+++ MicrocontrollerLab/Lab1/playGround/playGround/playGround/main.c
@@ -0,0 +1,18 @@
+/*
+ * playGround.c
+ *
+ * Created: 04/04/2021 9:48:07 pm
+ * Author : luonc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
