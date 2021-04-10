6c2b96ada144d00d17e157b4cca0c01566673846
Jake Chung <luonchung@gmail.com>
2021-04-10 16:14:24 -0700

    Matlab class for making opamp circuit

---
+++ ControlLab/MatlabLib/makeOpAmpCircuits.m
@@ -0,0 +1,26 @@
+classdef makeOpAmpCircuits
+    %Class to create filters of different kinds
+    properties
+        R
+        C
+        A %gain
+    end
+    methods (Static)
+        function R2 = makeInv(A, R1)
+            %function to calculate R2 for Inverting opamp circuit
+            if A > 0
+                R2 = A*R1;
+            else
+                R2 = -A*R1;
+            end
+        end
+        function R2 = makeNonInv(A, R1)
+            %function to calculate R2 for non inverting opamp circuit
+            if A < 1
+                fprintf('Non-inverting opamp, A cannot be less than 1')
+                return
+            end
+            R2 = R1*(A-1);
+        end
+    end
+end
