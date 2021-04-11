13d8c6e737fb43539b2a647a0c1e1bd677fe174d
Jake Chung <luonchung@gmail.com>
2021-04-11 15:43:11 -0700

    change to 16MHz external clock

--- MicrocontrollerLab/Lab1/Blink2Program/Blink2Program/main.c
+++ MicrocontrollerLab/Lab1/Blink2Program/Blink2Program/main.c
@@ -1,6 +1,6 @@
 // Blink Version 2
 #include <avr/io.h>
-#define F_CPU 1000000UL  // 1 MHz  CPU Clock Frequency
+#define F_CPU 16000000UL  // 16 MHz  CPU Clock Frequency
 #include <util/delay.h>  // Include the built in Delay Function
 #define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask)) //set bit in I/O register
 #define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask)) //clear bit in I/O register
