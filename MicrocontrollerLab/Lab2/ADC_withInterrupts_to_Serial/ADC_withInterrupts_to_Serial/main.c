5f8dcd54d59d0d542a8453f0e963ce23927108a5
Jake Chung <luonchung@gmail.com>
2021-04-11 18:33:20 -0700

    commit for ACD with interrupts to Serial

---
+++ MicrocontrollerLab/Lab2/ADC_withInterrupts_to_Serial/ADC_withInterrupts_to_Serial/main.c
@@ -0,0 +1,18 @@
+/*
+ * ADC_withInterrupts_to_Serial.c
+ *
+ * Created: 11/04/2021 6:26:34 pm
+ * Author : luonc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
