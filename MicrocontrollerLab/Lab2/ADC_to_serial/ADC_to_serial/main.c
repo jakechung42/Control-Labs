9ee1e48a6eef9db5653876ae8d69236b7e1f91d2
Jake Chung <luonchung@gmail.com>
2021-04-11 17:54:27 -0700

    ADC write to serial communication

---
+++ MicrocontrollerLab/Lab2/ADC_to_serial/ADC_to_serial/main.c
@@ -0,0 +1,18 @@
+/*
+ * ADC_to_serial.c
+ *
+ * Created: 11/04/2021 5:53:20 pm
+ * Author : luonc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
