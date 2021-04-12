e30a6029cd3fd2bb3b7aebc70b1dc416a4e85991
Jake Chung <luonchung@gmail.com>
2021-04-11 18:14:10 -0700

    initial commit for ADC with interrupts

---
+++ MicrocontrollerLab/Lab2/ADC_with_Interrupts/ADC_with_Interrupts/Debug/main.d
@@ -0,0 +1,40 @@
+main.d main.o: .././main.c \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88a.h \
+ O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88a.h:
+
+O:\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom88.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+o:\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
---
+++ MicrocontrollerLab/Lab2/ADC_with_Interrupts/ADC_with_Interrupts/main.c
@@ -0,0 +1,45 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define LEDs	PORTD
+#define red		0b11011111
+#define green	0b01111111
+#define yellow	0b10111111
+
+ISR(ADC_vect)
+{
+	unsigned int adc_data;
+
+	adc_data = ADCW;
+
+	if(adc_data > (3*1023)/5)
+	{
+		LEDs = red;
+	}
+	else if (adc_data < (2*1023)/5)
+	{
+		LEDs = yellow;
+	}
+	else
+	{
+		LEDs = green;
+	}
+	ADCSRA = ADCSRA | 0b01000000;
+}
+
+int main (void)
+{
+	DDRD   = 0b11100000; //Set bits 6, 7 and 8 as outputs
+	ADMUX  = 0b00000000; //Input on AD Channel 0
+	ADCSRA = 0b11001111; // ADC on, /128 for a 20 MHz clock, interrupt on
+
+	asm("sei");
+
+	while(1)
+	{
+		;
+	}
+}
+
+
+
