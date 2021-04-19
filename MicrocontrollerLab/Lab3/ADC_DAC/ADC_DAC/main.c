bee26454ab36f269e8bcd16e408988940927c93e
Jake Chung <luonchung@gmail.com>
2021-04-18 21:49:48 -0700

    working build to test ADC and DAC communication

--- MicrocontrollerLab/Lab3/ADC_DAC/ADC_DAC/main.c
+++ MicrocontrollerLab/Lab3/ADC_DAC/ADC_DAC/main.c
@@ -19,17 +19,27 @@ int main (void)
 	unsigned char   spi_data_0;
 	unsigned char   spi_data_1;
 	unsigned char   dummy_read;
-	unsigned int	adc_output = 0x0000;
+	unsigned int	adc_output;
 
 	DDRB=0b00101100;	//Set Output Ports for the SPI Interface
 	DDRD=0b10000000;	//Set Output Ports for the Chip select
 
 	// SPI initialization
 	SPCR=0b01010010;
-	SPSR=0b00000000;
-	
+	SPSR=0b00000001;
+
+	//ADC set up
+	ADMUX  = 0b00000000; //Input on AD Channel 0
+	ADCSRA = 0b10000111; // ADC on, /128 for a 16 MHz clock, interrupt off
+
 	while(1)
 	{
+		//ADC process
+		ADCSRA = ADCSRA | 0b01000000;  // Start AD conversion, this or gate only flips ADSC
+		while ((ADCSRA & 0b01000000) == 0b01000000); // Wait while AD conversion is executed
+		adc_output = ADCW * 4;
+
+		//SPI process
 		spi_data_0 = 0x00;
 		spi_data_0 = (adc_output & 0x0F00) >> 8;  //Set up first byte to write
 		spi_data_0 = spi_data_0 + 0b00110000;
@@ -39,14 +49,5 @@ int main (void)
 		dummy_read = spi_write_read(spi_data_0);	// Write/Read first byte
 		dummy_read = spi_write_read(spi_data_1);  	// Write/Read second byte
 		sbi(PORTD,7);								// Release the chip  - set chip select to one
-
-		if (adc_output==0x1000) //condition to reset spi_data
-		{
-			adc_output = 0;
-		}
-		else
-		{
-			adc_output += 1;
-		}
 	}
 }
