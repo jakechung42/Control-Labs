f503a8a8106d310d34bd665a3bc6bb69e67dbd63
Jake Chung <luonchung@gmail.com>
2021-04-19 21:04:40 -0700

    lag controller data and lag controller raw data

--- ControlLab/Lab3/lead_lag_plotting.m
+++ ControlLab/Lab3/lead_lag_plotting.m
@@ -14,3 +14,47 @@ num = [R1*R2*R4*C1 R2*R4];
 den = [R1*R2*R3*C2 R1*R3];
 sys = tf(num, den)
 bode(sys)
+
+%% Set up the theoretical system transfer function for Lead circuit
+R1 = 8.192e3;
+R2 = 22.078e3;
+R3 = 17.966e3;
+R4 = 22.008e3;
+C1 = 68e-9;
+C2 = 0.22e-6;
+num = [R1*R2*R4*C1 R2*R4];
+den = [R1*R2*R3*C2 R1*R3];
+sys = tf(num, den)
+
+%% Input Lag circuit experimental circuit
+% Input the experimental data
+%read data from .dat file
+data = load('Lag_data.dat');
+f_data = data(:, 1);
+mag_data = data(:, 2);
+phase_data = data(:, 3);
+%convert frequency to angular frequency
+w_data = f_data*2*pi;
+
+%Use the Bode command to calculate the theoretical magnitude, phase of the system
+[mag,phase,w]=bode(sys);
+% bode(sys)
+
+% The magnitude and phase are 3 dimensional arrays from the Bode command
+% and must be converted to 1 dimensional arrays using the squeeze command
+magp = squeeze(mag);
+phasep = squeeze(phase);
+% Manually plot the theoretical and experimental data on the bode plot
+figure
+subplot(2,1,1)
+semilogx(w,20*log10(magp),w_data,20*log10(mag_data),'o')
+xlabel('Frequency (rad/sec)')
+ylabel('Magnitude(db)')
+xlim([10 10^4])
+title('PD circuit')
+subplot(2,1,2)
+semilogx(w,phasep,w_data,phase_data,'o')
+xlabel('Frequency (rad/sec)')
+ylabel('Phase(deg)')
+xlim([10 10^4])
+ylim([-60 0])
