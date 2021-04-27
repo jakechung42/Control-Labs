8ab9910030042ac2d4b539b5b0b4c71f67ec4ffd
Jake Chung <luonchung@gmail.com>
2021-04-26 20:23:28 -0700

    initial creation for the PWM DC Motor code

---
+++ MicrocontrollerLab/Lab4/PWM_DC_Motor/PWM_DC_Motor/main.c
@@ -0,0 +1,18 @@
+/*
+ * PWM_DC_Motor.c
+ *
+ * Created: 26/04/2021 8:22:47 pm
+ * Author : luonc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
