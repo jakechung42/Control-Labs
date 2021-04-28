bd610ae53daba7993c49386b282f9419211f79af
Jake Chung <luonchung@gmail.com>
2021-04-28 00:03:28 -0700

    update build for PWM servo control and Timer_Interrupt

--- MicrocontrollerLab/Lab4/Timer_Interrupt/Timer_Interrupt/main.c
+++ MicrocontrollerLab/Lab4/Timer_Interrupt/Timer_Interrupt/main.c
@@ -5,38 +5,33 @@
 
 int main (void)
 {
-	DDRC = (1 << 5); // Set Pin 5 on Port C as output
+DDRC = (1 << 5); // Set Pin 5 on Port C as output
 
-	TCCR1B |= (1 << WGM12); // Configure timer 1 for CTC mode
+TCCR1B |= (1 << WGM12); // Configure timer 1 for CTC mode
 
-	TIMSK1 |= (1 << OCIE1A); // Enable CTC interrupt
+TIMSK1 |= (1 << OCIE1A); // Enable CTC interrupt
 
-	sei(); // Enable global interrupts
+sei(); // Enable global interrupts
 
-	// OCR1A = Target_Timer_Count = (Clock_Frequency / (Prescale * Target_Frequency)) - 1
+// OCR1A = Target_Timer_Count = (Clock_Frequency / (Prescale * Target_Frequency)) – 1
 
-	OCR1A = 31;   //Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
+OCR1A = 15624;   //Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
 
-	//Prescaler Fcpu/1 to get 1kHz and 5kHz counter
-	TCCR1B = TCCR1B | (1 << CS10);
+TCCR1B |= ((1 << CS10) | (1 << CS11)); // Start timer at Fcpu/64
 
-	// Timer at Fcpu/256 prescaler to get the 1Hz clock
-	//TCCR1B = TCCR1B | (1 << CS12); 
-
-	// TCCR1B |= ((1 << CS10) | (1 << CS11)); // Start timer at Fcpu/64 
-
-	while(1){}
+while(1){}
 
 }
 
 ISR(TIMER1_COMPA_vect)
 {
-	// Place code here that should be executed when the interrupt occurs
+// Place code here that should be executed when the interrupt occurs
 
-	PORTC ^= (1 << 5); // Toggle Pin 5 on Port C
+PORTC ^= (1 << 5); // Toggle Pin 5 on Port C
 }
 
 
 
 
 
+ 
