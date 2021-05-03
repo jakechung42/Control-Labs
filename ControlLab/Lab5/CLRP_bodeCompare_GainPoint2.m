37c9489cfb1849281d71cdab3156af92ddd333bb
Jake Chung <luonchung@gmail.com>
2021-05-02 20:49:57 -0700

    update raw data and plotting code for the gain controller 0.2 gain

---
+++ ControlLab/Lab5/CLRP_bodeCompare_GainPoint2.m
@@ -0,0 +1,37 @@
+% plot the 0.2 theoretical and experimental data to compare the built
+% circuit and the theoretical circuit
+
+clear
+close all
+clc
+
+%% Read the raw data
+path = "D:\Github\Control-Labs\ControlLab\Lab5\CL_rawData_Bode.dat";
+raw_data = load(path, '-ascii');
+freq_exp = raw_data(:,1);
+mag_exp = raw_data(:,2);
+phase_exp = raw_data(:,3);
+
+%% derived bode model
+K = 0.2;
+num = K*2.388E5;
+den = [1 1728 1.194E5+K*2.388E5];
+sys = tf(num, den);
+w = logspace(-1, 3);
+[mag_t, phase_t] = bode(sys, w);
+mag_t = squeeze(mag_t);
+phase_t = squeeze(phase_t);
+f = w/(2*pi);
+
+%% Bode plot the raw data
+figure
+subplot(2,1,1)
+semilogx(freq_exp*2*pi, 20*log10(mag_exp), 'o')
+hold on
+semilogx(w, 20*log10(mag_t))
+subplot(2,1,2)
+semilogx(freq_exp*2*pi, phase_exp, 'o')
+hold on
+semilogx(w, phase_t)
+
+
