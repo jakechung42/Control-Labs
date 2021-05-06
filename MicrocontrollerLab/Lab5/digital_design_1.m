c5b88ea688deac6608796c3f75d20995ef2cb4f5
Jake Chung <luonchung@gmail.com>
2021-05-05 21:44:48 -0700

    initial commit for designing a digital controller

---
+++ MicrocontrollerLab/Lab5/digital_design_1.m
@@ -0,0 +1,14 @@
+%this code designs the degital system to make the Lead Controller.
+clc
+clear
+close all
+
+%define the motor system from control lab
+s = tf('s');
+Gp = 2.388E5/(0.002*s^3+3.765*s^2+1621*s+1.194E5);
+Ts = 0.001;
+sysd_Gp = c2d(Gp, Ts, 'ZOH');
+
+%find the w transform
+sysw_Gp = d2c(sysd_Gp, 'tustin')
+bode(sysw_Gp)
