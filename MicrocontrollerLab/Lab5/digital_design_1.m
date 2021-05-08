3dca61d7fdbd485e1d65b6e689fb08a2f024818d
Jake Chung <luonchung@gmail.com>
2021-05-08 14:10:46 -0700

    controller digital design

--- MicrocontrollerLab/Lab5/digital_design_1.m
+++ MicrocontrollerLab/Lab5/digital_design_1.m
@@ -6,9 +6,19 @@ close all
 %define the motor system from control lab
 s = tf('s');
 Gp = 2.388E5/(0.002*s^3+3.765*s^2+1621*s+1.194E5);
-Ts = 0.001;
-sysd_Gp = c2d(Gp, Ts, 'ZOH')
+Ts = 0.001; %1k Hz sampling frequency
+sysD = c2d(Gp, Ts, 'ZOH') %convert to digital system 
 
 %find the w transform
-sysw_Gp = d2c(sysd_Gp, 'tustin')
-bode(sysw_Gp)
+K = 1; %let's simplify and go with a K=1 for now
+sysw = d2c(K*sysD, 'tustin') %this is actually the w transform.
+bode(sysw)
+margin(sysw)
+grid on
+
+%uncompensated step response
+sysD_cl = feedback(K*sysD, 1)
+%step response of uncompensated system
+figure
+step(sysD_cl)
+grid on
