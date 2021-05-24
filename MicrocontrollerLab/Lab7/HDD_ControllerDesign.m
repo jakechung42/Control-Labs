25c7d2faa48e526c8f06727d4ac8fd656e145418
Jake Chung <luonchung@gmail.com>
2021-05-23 20:57:00 -0700

    update simulink data

--- MicrocontrollerLab/Lab7/HDD_ControllerDesign.m
+++ MicrocontrollerLab/Lab7/HDD_ControllerDesign.m
@@ -27,7 +27,7 @@ figure
 step(cl_sysD_pp)
 
 %% Lead controller design
-K = 1; %for running Lead
+K = 2; %for running Lead
 sysw = d2c(K*ol_sysD, 'tustin')  %this is actually the w transform
 figure
 margin(sysw)
@@ -53,34 +53,60 @@ fprintf('Closed loop response to lead controller')
 stepinfo(sysD_cl_lead)
 
 %% read the raw data
-% path = "D:\Github\Control-Labs\MicrocontrollerLab\Lab7\Lead_controller_scope_data.csv";
-% raw_scope_data = csvread(path, 2, 0);
-% scope_time = raw_scope_data(:,1);
-% scope_input = raw_scope_data(:,2);
-% scope_output = raw_scope_data(:,3);
-% 
-% %scle and shift the data
-% scope_input = scope_input - min(scope_input);
-% scope_output = scope_output - min(scope_output);
-% %plot raw data
-% figure
-% plot(scope_time, scope_input)
-% hold on
-% plot(scope_time, scope_output)
-% title('Raw scope data')
-% xlabel('Time (s)')
-% ylabel('Voltage(V)')
-% 
-% idx = find(scope_time >= 0 & scope_time <0.227);
-% scope_time = scope_time(idx);
-% scope_input = scope_input(idx);
-% scope_output = scope_output(idx);
-% scope_time_mod = linspace(0, max(scope_time), length(scope_time));
-% model_lead_sys = lsim(sysD_cl_lead, scope_input, scope_time_mod);
-% 
-% %plot the model vs scope data
-% figure
-% plot(scope_time_mod, model_lead_sys)
-% hold on
-% plot(scope_time, scope_output, 'd')
-% title('Comparing the step response of system with lead controller')
+path = "D:\Github\Control-Labs\MicrocontrollerLab\Lab7\Lead_controller_scope_data_2.csv";
+raw_scope_data = csvread(path, 2, 0);
+scope_time = raw_scope_data(:,1);
+scope_input = raw_scope_data(:,2);
+scope_output = raw_scope_data(:,3);
+
+%scle and shift the data
+scope_input = scope_input - min(scope_input);
+scope_output = scope_output - min(scope_output);
+scope_output = scope_output / (max(scope_input) - min(scope_input));
+%plot raw data
+figure
+plot(scope_time, scope_input)
+hold on
+plot(scope_time, scope_output)
+title('Raw scope data')
+xlabel('Time (s)')
+ylabel('Voltage(V)')
+
+idx = find(scope_time >= 0 & scope_time <0.182);
+scope_time = scope_time(idx);
+scope_input = scope_input(idx);
+scope_output = scope_output(idx);
+[model_lead_sys, model_lead_time] = step(sysD_cl_lead);
+
+%plot the model vs scope data
+figure
+plot(model_lead_time, model_lead_sys)
+hold on
+plot(scope_time, scope_output, 'd')
+title('Comparing the step response of system with lead controller')
+
+%% read simulink simulated for the Lead controller
+simulink_data_struct = load('LeadStepResponse.mat', '-mat', 'LeadStepResponse');
+sl_data = simulink_data_struct.LeadStepResponse;
+%plot the raw Simulink data
+sl_time = sl_data(:,1);
+sl_control_signal = sl_data(:,2);
+sl_response = sl_data(:,3);
+figure
+plot(sl_time, sl_control_signal)
+hold on
+plot(sl_time, sl_response)
+title('Simulink data with saturation')
+ylabel('Voltage')
+xlabel('Time')
+grid on
+
+%plot to compare with scope data
+figure
+plot(sl_time, sl_response)
+hold on
+plot(scope_time, scope_output, 'd')
+ylabel('Voltage')
+xlabel('Time')
+axis([0, 0.2, 0, 1])
+title('Comparing the scope response with simulink data')
