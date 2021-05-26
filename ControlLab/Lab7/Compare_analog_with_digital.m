65be46e773dbc94771166d7a13f3f48feac93e2a
Jake Chung <luonchung@gmail.com>
2021-05-25 22:53:23 -0700

    update

--- ControlLab/Lab7/Compare_analog_with_digital.m
+++ ControlLab/Lab7/Compare_analog_with_digital.m
@@ -48,14 +48,41 @@ title('Shifted digital proportional controller response')
 ylabel('Voltage')
 xlabel('Time')
 
+%% Read raw data of the step response for the 1.25 gain controller
+path = "D:\Github\Control-Labs\ControlLab\Lab7\gainController_1.2.csv";
+data = csvread(path, 2, 0);
+t_step = data(:,1);
+V_in_step = data(:,2);
+V_out_step = data(:,3);
+%plot raw
+figure
+plot(t_step, V_in_step, t_step, V_out_step)
+legend('Input signal', 'Output signal')
+xlabel('Time (s)')
+ylabel('Voltage (V)')
+%shift and scale the data
+pos_idx = find(t_step > 0.133 & t_step < 0.251);
+t_step = t_step(pos_idx);
+t_step = t_step - 0.134;
+V_in_step_amp = max(V_in_step) - min(V_in_step);
+V_in_step = V_in_step(pos_idx);
+V_in_step = V_in_step - min(V_in_step);
+V_out_step = V_out_step(pos_idx);
+V_out_step = V_out_step - min(V_out_step);
+V_out_step = V_out_step/V_in_step_amp;
+figure
+plot(t_step, V_out_step)
+
 %% compare the digital response and modeled response
-[model_response, model_time] = step(sys);
+model_time = linspace(0, 0.12, 5000);
+[model_response] = step(sys, model_time);
 figure %compare the raw data and modeled response
 plot(model_time, model_response)
 hold on
 plot(scope_time, scope_output, 'd')
-
+plot(t_step, V_out_step, 'd')
 legend('Model response', 'Digital controller output', 'Analog controller output')
 title('Comparing the analog model response with digital controller data')
 ylabel('Voltage')
 xlabel('Time')
+grid on
--- ControlLab/Lab7/Compare_analog_with_digital_PI.m
+++ ControlLab/Lab7/Compare_analog_with_digital_PI.m
@@ -49,13 +49,48 @@ title('Shifted digital proportional controller response')
 ylabel('Voltage')
 xlabel('Time')
 
+%% compare step response PI controller theoretical and experimental
+path = "D:\Github\Control-Labs\ControlLab\Lab5\PI_Controller_rawData_1.csv";
+
+A = csvread(path, 2, 0);
+
+t_step_raw = A(:,1);
+V_in_step_raw = A(:,2);
+V_out_step_raw = A(:,3);
+
+V_in_step_amp = max(V_in_step_raw) - min(V_in_step_raw);
+
+%plot raw data
+figure
+plot(t_step_raw, V_in_step_raw)
+hold on
+plot(t_step_raw, V_out_step_raw)
+%shift the raw data
+shift_idx = find(t_step_raw >= -1.544 & t_step_raw <0);
+t_step = t_step_raw(shift_idx);
+V_in_step = V_in_step_raw(shift_idx);
+V_out_step = V_out_step_raw(shift_idx);
+t_step = t_step + 1.544; %shift time to 0
+V_in_step = V_in_step + abs(min(V_in_step)); %shift to positive 
+V_out_step = V_out_step + abs(min(V_out_step)); %shift to positive
+V_out_step = V_out_step/V_in_step_amp;
+%plot the shifted data
+figure
+plot(t_step, V_in_step, t_step, V_out_step)
+xlabel('Time (s)')
+ylabel('Voltage (s)')
+legend('Input signal', 'Response')
+title('Shifted raw data from scope for PI controller implementation')
+grid on
+
 %% compare the digital response and modeled response
 [model_response, model_time] = step(control_clrp);
 figure %compare the raw data and modeled response
 plot(model_time, model_response)
 hold on
 plot(scope_time, scope_output, 'd')
+plot(t_step, V_out_step, 'd')
 title('Comparing the analog model response with digital controller data')
 ylabel('Voltage')
 xlabel('Time')
-axis([0, 0.25, 0, 0.8])
+% axis([0, 0.25, 0, 0.8])
