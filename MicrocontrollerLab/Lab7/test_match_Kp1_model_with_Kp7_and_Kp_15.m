b929e42544372ea110ed21536441b3589e3668b4
Jake Chung <luonchung@gmail.com>
2021-05-29 14:40:49 -0700

    comparing the Kp1 model with Kp15 and Kp7 step response

---
+++ MicrocontrollerLab/Lab7/test_match_Kp1_model_with_Kp7_and_Kp_15.m
@@ -0,0 +1,77 @@
+%test to see if the Kp15 model matches with the Kp7 model
+clear
+clc
+close all
+
+%% define system
+s = tf('s');
+Kp = 7;
+ol_tf = 1.437e6/(s^3+484.5*s^2+38025*s+2.53e5)
+% sysD_ol = c2d(ol_tf, Ts, 'ZOH'); %let's compare the digital system
+cl_tf = feedback(Kp*ol_tf, 1);
+
+figure
+step(cl_tf)
+model = cl_tf;
+
+%% read raw step Kp7 data
+path = "D:\Github\Control-Labs\MicrocontrollerLab\Lab7\raw_step_Kp7.csv";
+raw_step = csvread(path, 2, 0);
+time_step_scope = raw_step(:,1);
+V_in_step_scope = raw_step(:,2);
+V_out_step_scope = raw_step(:,3);
+
+%plot the raw data
+figure
+plot(time_step_scope, V_in_step_scope, time_step_scope, V_out_step_scope, 'd')
+title('Raw step data')
+ylabel('Voltage (V)')
+xlabel('Time (s)')
+grid on
+
+path = "D:\Github\Control-Labs\MicrocontrollerLab\Lab7\raw_step_Kp15.csv";
+raw_step = csvread(path, 2, 0);
+time_step_scope_15 = raw_step(:,1);
+V_in_step_scope_15 = raw_step(:,2);
+V_out_step_scope_15 = raw_step(:,3);
+%scale and normalize and shift the data
+% min_vin = min(V_in_step_scope);
+% v_in_amp = max(V_in_step_scope)-min(V_in_step_scope);
+% idx = find(time_step_scope > 0 & time_step_scope < 0.153); %shift time
+% time_step_scope = time_step_scope(idx); 
+% V_in_step_scope = V_in_step_scope(idx);
+% V_in_step_scope = V_in_step_scope - min_vin;
+% V_out_step_scope = V_out_step_scope(idx);
+% V_out_step_scope = (V_out_step_scope - 0.336)/v_in_amp;
+t_step_mod_15 = linspace(min(time_step_scope_15), max(time_step_scope_15), length(time_step_scope_15));
+t_step_mod = linspace(min(time_step_scope), max(time_step_scope), length(time_step_scope));
+% [step_model, t_step_model] = step(model);
+[step_model, t_step_model] = lsim(model, V_in_step_scope, t_step_mod);
+[step_model_15, t_step_model_15] = lsim(model, V_in_step_scope_15, t_step_mod_15);
+
+% %plot the shifted data
+% figure 
+% plot(time_step_scope, V_in_step_scope, time_step_scope, V_out_step_scope, 'o')
+% title('Raw step data')
+% ylabel('Voltage (V)')
+% xlabel('Time (s)')
+% grid on
+%plot to compare step response of the model and raw data
+figure
+plot(t_step_model, step_model)
+hold on
+plot(time_step_scope, V_out_step_scope, '-')
+grid on
+ylabel('Voltage (V)')
+xlabel('Time (s)')
+title('Comparing theoretical step response of Kp = 1 model with Kp = 7 response data')
+
+% Kp 15
+figure
+plot(t_step_model_15, step_model_15)
+hold on
+plot(time_step_scope_15, V_out_step_scope_15, '-')
+grid on
+ylabel('Voltage (V)')
+xlabel('Time (s)')
+title('Comparing theoretical step response of Kp = 1 model with Kp = 15 response data')
