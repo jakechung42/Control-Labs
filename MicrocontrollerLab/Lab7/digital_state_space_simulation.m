fab9af4712b07808b8081e633ba80b64feebf06a
Jake Chung <luonchung@gmail.com>
2021-05-30 16:02:25 -0700

    update simulation for state space design

--- MicrocontrollerLab/Lab7/digital_state_space_simulation.m
+++ MicrocontrollerLab/Lab7/digital_state_space_simulation.m
@@ -17,7 +17,7 @@ sys_ss = ss(sys_ol);                            % Form the continuous state spac
                                                 % and the original continuous states (very convenient!)
                                                 % See c2d documentation for a full explanation of G. 
 
-% Verify the digital state space model
+%% Verify the digital state space model
 
 [step_pos,step_time] = step(sys_ss_d);   % Save values to compare with values calculate using the recursive equation
 
@@ -116,7 +116,7 @@ legend('Step function','Recursive equation')
 % ylabel('State variable 3')
 
 
-% Design a Standard State Space Controller
+%% Design a Standard State Space Controller
 
 % Design the controller
 
@@ -201,7 +201,7 @@ xlabel('Time (sec)')
 ylabel('Position (inch)')
 legend('Step function','Recursive equation')
 
-% Design and add an Observer
+%%  Design and add an Observer
 
 % Convert specified roots to digital roots
 
@@ -230,10 +230,11 @@ for ii=1:n_steps
 
     % Simulation Model (system states estimated from the theoritical model)
     F0            = Rin(ii) - K*xo_km1;  
-    xk_simulation = A*xkm1_simulation + B*F0
-    Ck = D*xk_simulation
+    xk_simulation = A*xkm1_simulation + B*F0;
+    Ck = D*xk_simulation;
 
-    Ck_save(ii) = Ck;
+    control_save(ii) = F0; %save the control signal
+    Ck_save(ii) = Ck; %response read from the encoder
     
     % Measurment Simulation (This would be directly measured in the arctual implimentation)
     Co_k = [(Ck + noise_x3(ii))];  % Note Measurment comes from the simulation here
@@ -268,6 +269,12 @@ figure
 stairs(time,Co_save)
 title({'Measurement with noise','Standard State Space Controller - With Observer'})
 
+figure %plot the control signal
+stairs(time, control_save)
+title('Control signal')
+ylabel('Voltage')
+xlabel('Time')
+
 
 % Note when the observer code above is implemented on a microcontroller to
 % estimate the systems unmeasured states all of the matrix and vector
@@ -277,28 +284,29 @@ title({'Measurement with noise','Standard State Space Controller - With Observer
 % being done each time through the loop and slowing the system down.
 
 
-%{
-% Design a State Space Controller with an outer feedback loop and an integrator
+
+%% Design a State Space Controller with an outer feedback loop and an integrator
 
 % Design the controller
 
-p=[-10 -12 -14];
+p=[-a -a*2 -a*2.5 -a*2.8]; %extra root for the integrator 
 % Convert specified roots to digital roots
 
-pd=[exp(p(1)*Ts) exp(p(2)*Ts) exp(p(3)*Ts)]; % Digital Controller roots
+pd=[exp(p(1)*Ts) exp(p(2)*Ts) exp(p(3)*Ts) exp(p(4)*Ts)]; % Digital Controller roots
 
 % See "Handout 26 Outer Feedback Loop with an Integrator for Digital State Space Control"
 
 Ahat = [A B;zeros(length(A),1)' 0]
 Bhat = [zeros(length(A),1);1]
+% Bhat = [0; ones(length(A),1)] %DON'T USE THIS, FOR TESTING ONLY (not sure why but the handout said to use this)
 Khat = place(Ahat,Bhat,pd)
 
 [Khat + [zeros(length(A),1)' 1]]
 AA = [A-eye(size(A)) B;D*A D*B]
 
-KK = [Khat + [zeros(length(A),1)' 1]]*inv([A-eye(size(A)) B;D*A D*B])
-K2 = KK(1:end-1)
-K1 = KK(end)
+KK = [Khat + [zeros(length(A),1)' 1]]*inv([A-eye(size(A)) B;D*A D*B]) %the output of this is [K] and Ki
+K2 = KK(1:end-1) %[K] original
+K1 = KK(end) %Ki for the integrator component
 ACL = [A-B*K2 B*K1;-D*A+D*B*K2 1-D*B*K1]
 BCL = Bhat
 DCL = [D 0]
@@ -306,47 +314,55 @@ sys1 = ss(ACL,BCL,DCL,0,Ts)
 
 [step_pos_cli,step_time_cli] = step(sys1,n_steps*Ts);   % Save values to compare with values calculate using the recursive equation
 
+%del me when done
+figure 
+step(sys1,n_steps*Ts)
+title('Step response - assume perfect observer - check integrator implementation')
+
 % Simulate the response using recursive equations
 
-xk = zeros(length(DCL),1);  % Initial values of the state variables
+% xk = zeros(length(DCL),1);  % Initial values of the state variables
 
-for k= 1:n_steps
-    % Save data for ploting
-    x1_save(k) = xk(1);
-    x2_save(k) = xk(2);
-    x3_save(k) = xk(3);
+% for k= 1:n_steps
+%     % Save data for ploting
+%     x1_save(k) = xk(1);
+%     x2_save(k) = xk(2);
+%     x3_save(k) = xk(3);
     
-    % Recursive calculation of the closed loop response with state feedback
-    F0   = 1.0;
-    xkp1 = ACL*xk+BCL*F0;
+%     % Recursive calculation of the closed loop response with state feedback
+%     F0   = 1.0;
+%     xkp1 = ACL*xk+BCL*F0;
     
-    % Update the System States
-    xk = xkp1;
-end
+%     % Update the System States
+%     xk = xkp1;
+% end
 
-figure
-stairs(step_time_cli,step_pos_cli)
-hold on
-stairs(time,x1_save)
-title({'Closed Loop Position step response','Integrator feedback - recursive equations - No Observer (all states perfectly known)'})
-xlabel('Time (sec)')
-ylabel('Position (inch)')
-legend('Step function','Recursive equation')
-xlim([0 2]);
-ylim([0 1.2]);
+% figure
+% stairs(step_time_cli,step_pos_cli)
+% hold on
+% stairs(time,x1_save)
+% title({'Closed Loop Position step response','Integrator feedback - recursive equations - No Observer (all states perfectly known)'})
+% xlabel('Time (sec)')
+% ylabel('Position (inch)')
+% legend('Step function','Recursive equation')
+% xlim([0 2]);
+% ylim([0 1.2]);
 
 
-% Design and add an Observer
+%% Design and add an Observer
 
 % Convert specified roots to digital roots
 
 % pd_ob=[exp(1*p(1)*Ts) exp(1*p(2)*Ts)];     % Digital Observer roots 1* Controller roots
 % pd_ob=[exp(5*p(1)*Ts) exp(5*p(2)*Ts)];     % Digital Observer roots 5* Controller roots
-pd_ob=[exp(10*p(1)*Ts) exp(10*p(2)*Ts)];     % Digital Observer roots 10* Controller roots
+% pd_ob=[exp(10*p(1)*Ts) exp(10*p(2)*Ts)];     % Digital Observer roots 10* Controller roots
 % pd_ob=[exp(20*p(1)*Ts) exp(20*p(2)*Ts)];   % Digital Observer roots 20* Controller roots
 % pd_ob=[exp(100*p(1)*Ts) exp(100*p(2)*Ts)]; % Digital Observer roots 100* Controller roots
+% p = [-2*a -2.5*a -3*a];
+pb_ob = [exp(20*p(1)*Ts) exp(20*p(2)*Ts) exp(20*p(3)*Ts)]
 
 L=place(A',D',pd_ob);
+fprintf('\nL matrix with integrator:')
 L=L'
 
 % Full order Observer
@@ -357,10 +373,13 @@ xoI_km1                = 0.0;
 Co_km1                 = 0.0;
 Rin(1:n_steps)         = 1.0;  %Step input
 
-noise_C_magnitude = 0.005;                                      % Mesurment noise magnitude
+noise_C_magnitude = 0.01;                                      % Mesurment noise magnitude
 noise_x1          = noise_C_magnitude*normrnd(0,1,1,n_steps);   % Noise defined as a normal distribution
-
+A1 = (A - B*K2 - L*D)
 % Simulate the closed loop response with an observer using recursive equations
+
+%K2: K vector for state space
+%K1: Ki
 
 for ii=1:n_steps
     
@@ -368,17 +387,25 @@ for ii=1:n_steps
     xo_save{ii}              = xo_km1;
     xoI_save(ii)             = xoI_km1;
     x_simulation_FO_save{ii} = xkm1_simulation;  % Save Simulation SV's
+    Ck_out_save(ii) = Co_km1;
 
     % Simulation Model (system states estimated from the theoritical model)
     
-    F0               =  - K2*[xo_km1(1);xo_km1(2)] + K1*xkm1_simulation(3);  
-    zz               = A*[xkm1_simulation(1);xkm1_simulation(2)] + B*F0;
+    %uk = Ki*~xk - [K]*{xk}
+    F0 = -K2*[xo_km1(1);xo_km1(2);xo_km1(3)] + K1*xkm1_simulation(4); %control feedback
+    %{x(k+1)} = [A]{xk} + [B]uk
+    zz = A*[xkm1_simulation(1);xkm1_simulation(2);xkm1_simulation(3)] + B*F0; %process equation
+
+    %rebuilt the vector for observer
     xk_simulation(1) = zz(1);
     xk_simulation(2) = zz(2);
-    xk_simulation(3) = xkm1_simulation(3) + (Rin(ii) - xkm1_simulation(1));      % Integrator feedback term
-    
-    % Measurment Simulation (This would be directly measured in the arctual implimentation)
-    Co_k = [(xk_simulation(1) + noise_x1(ii))];  % Note Measurment comes from the simulation here
+    xk_simulation(3) = zz(3);
+
+    % Measurment Simulation (This would be directly measured in the actual implimentation)
+    Co_k = [(D(3)*xk_simulation(3) + noise_x1(ii))];  % Note Measurment comes from the simulation here
+
+    %~xk = ~x(k-1) + Rk - Ck
+    xk_simulation(4) = xkm1_simulation(4) + (Rin(ii) - Co_k);      % Integrator feedback term
     
     % Recursive estimation of the closed loop states using a full order observer
     xo_k   = (A - B*K2 - L*D)*xo_km1 + B*K1*xoI_km1 + L*Co_km1; 
@@ -412,38 +439,26 @@ figure
 plot(time,x_simulation_FO_save_M(1,:),time,xo_save_M(1,:))
 title('State 1')
 xlabel('Time (sec)')
-ylabel('Position')
 legend('Simulation','Observer')
-xlim([0 2]);
 
 figure
 plot(time,x_simulation_FO_save_M(2,:),time,xo_save_M(2,:))
 title('State 2')
 xlabel('Time (sec)')
-ylabel('Velocity (inch/sec)')
 legend('Simulation','Observer')
-xlim([0 2]);
 
 figure
 plot(time,x_simulation_FO_save_M(3,:),time,xoI_save(1,:))
 title('State 3')
 xlabel('Time (sec)')
-ylabel('Integrator State')
 legend('Simulation','Observer')
-xlim([0 2]);
 
 figure
 stairs(step_time_cli,step_pos_cli)
 hold on
-stairs(time,xo_save_M(1,:))
+stairs(time,Ck_out_save)
 title({'Closed Loop Position step response','Integrator feedback - recursive equations - With Observer'})
 xlabel('Time (sec)')
 ylabel('Position (inch)')
 legend('Step function (No Observer - all states perfectly known)','Recursive equation - With Observer')
-xlim([0 2]);
-ylim([0 1.2]);
-
-%}
-
-
 
